88cc6d850f372e75280dd94ac4358d737d4fd8bd
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-02-25 22:08:50 +0100

    test: list locators tasks added

---
+++ tests/tasks/task_2/locator_list.spec.ts
@@ -0,0 +1,31 @@
+import { SimpleMultipleElementsPage } from '@_src/pages/simple-multiple-elements.page';
+import { test, expect } from '@playwright/test';
+
+test.describe('List of locators handling', () => {
+  let simpleMultipleElementsPage: SimpleMultipleElementsPage;
+  test.beforeEach('Navigate to proper page', async ({ page }) => {
+    simpleMultipleElementsPage = new SimpleMultipleElementsPage(page);
+    await simpleMultipleElementsPage.navigateTo();
+  });
+  test('Check All existed checkboxes', async ({ page }) => {
+    // Arrange
+    const checkboxSelector = 'checkbox';
+    const resultsSelector = 'dti-results';
+    const quantityOfExistedCheckboxes = 5;
+    // const resultMessage =
+    // Checkbox is checked! (Opt 1!)
+    const resultsLocator = page.getByTestId(resultsSelector);
+    const checkboxLocator = page.getByRole(checkboxSelector);
+
+    // Act
+    const checkboxesQuantity = await checkboxLocator.count();
+    expect.soft(checkboxesQuantity).toBe(quantityOfExistedCheckboxes);
+    // console.log('Quantity of checkboxes:', await checkboxesQuantity);
+    // Assert
+    for (let i = 0; i < checkboxesQuantity; i++) {
+      await checkboxLocator.nth(i).check();
+      //   console.log(await resultsLocator.textContent());
+      await expect(resultsLocator).toHaveText(`Checkbox is checked! (Opt ${i + 1}!)`);
+    }
+  });
+});
