284c3b328e6667c40f0cb37549a5d67b8dea1a25
Kruchy1980 <jakub.kruszynski@vp.pl>
2025-02-26 21:45:03 +0100

    chore: auto-waiting added

--- tests/5.adv_pw_concepts/auto-waiting.spec.ts
+++ tests/5.adv_pw_concepts/auto-waiting.spec.ts
@@ -20,4 +20,33 @@ test.describe('Auto waiting in PW', () => {
     //Assert
     await expect(resultsLocator).toHaveText(expectedMessage);
   });
+  test('2. Button visibility - Web First Assertion', async ({ page }) => {
+    //Arrange
+    const buttonTestId = 'dti-button-element';
+    // const resultsTestId = 'dti-results';
+    // const expectedMessage = 'You clicked the button!';
+    // Locators
+    const buttonLocator = page.getByTestId(buttonTestId);
+    // const resultsLocator = page.getByTestId(resultsTestId);
+    //Act
+    // await buttonLocator.click();
+    //Assert
+    await expect(buttonLocator).toBeVisible();
+  });
+  test('3. Button visibility attribute WFA (Web First Assertion)- not recommended', async ({
+    page,
+  }) => {
+    //Arrange
+    const buttonTestId = 'dti-button-element';
+    // const resultsTestId = 'dti-results';
+    // const expectedMessage = 'You clicked the button!';
+    // Locators
+    const buttonLocator = page.getByTestId(buttonTestId);
+    // const resultsLocator = page.getByTestId(resultsTestId);
+    const elementVisibility = buttonLocator.isVisible(); // Here we are waiting for the element first do not contain autoWait
+    //Act
+    // await buttonLocator.click();
+    //Assert
+    expect(elementVisibility).toBeTruthy();
+  });
 });
